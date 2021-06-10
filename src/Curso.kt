3028768e4e6a5df55a374d555714098c17fac64e
victor saccucci <victorsaccucci@hotmail.com>
2021-06-10 20:18:33 -0300

    função adicionarAluno e excluirAluno adicionadas a classe Curso.

--- src/Curso.kt
+++ src/Curso.kt
@@ -1,16 +1,26 @@
 class Curso(
     val nome: String,
-    val codigoDoCurso: Int
+    val codigoDoCurso: Int,
     val professorTitular: ProfessorTitular,
     val professorAdjunto: ProfessorAdjunto,
     val qtdMaxima: Int,
-    val listaAlunos: MutableList<Aluno> = mutableListOf()
+    private var listaAlunos: MutableList<Aluno> = mutableListOf()
 ) {
 
     override fun equals(other: Any?): Boolean {
         return (other is Curso) && this.codigoDoCurso == other.codigoDoCurso
 
     }
+
+    fun adicionarAluno(aluno: Aluno): Boolean {
+        if (listaAlunos.size < qtdMaxima) {
+            listaAlunos.add(aluno)
+            return true
+        }
+        return false
+    }
+
+    fun excluirAluno(aluno: Aluno){
+        listaAlunos.remove(aluno)
+    }
 }
-
-
