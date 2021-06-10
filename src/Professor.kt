abddccf624cd4994bccf1723ad134424a1d8dd0e
victor saccucci <victorsaccucci@hotmail.com>
2021-06-10 15:15:03 -0300

    classe professor alterada para abstrata. Criada as classes ProfessorTitular e ProfessorAdjunto.
    class ProfessorAdjunto e ProfessorTitular herdam da classe Professor.

--- src/Professor.kt
+++ src/Professor.kt
@@ -1,11 +1,32 @@
-class Professor (
-    val nome: String,
-    val sobrenome: String,
-    tempoDeCasa: Int,
-    val codigoDoProfessor: Int
-){
+abstract class Professor {
+    abstract val nome: String
+    abstract val sobrenome: String
+    abstract val tempoDeCasa: Int
+    abstract val codigoDoProfessor: Int
+
     override fun equals(other: Any?): Boolean {
         return (other is Professor) && this.codigoDoProfessor == other.codigoDoProfessor
     }
+}
+
+class ProfessorTitular(
+    override val nome: String,
+    override val sobrenome: String,
+    override val tempoDeCasa: Int,
+    override val codigoDoProfessor: Int,
+    val especialidade: String
+) : Professor() {
 
 }
+
+class ProfessorAdjunto(
+    override val nome: String,
+    override val sobrenome: String,
+    override val tempoDeCasa: Int,
+    override val codigoDoProfessor: Int,
+    val hosrasDeMonitoria: Int
+) : Professor() {
+
+}
+
+
