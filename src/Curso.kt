84b6c7d026fc680e4645d1926bb84869d65800c3
victor saccucci <victorsaccucci@hotmail.com>
2021-06-11 16:28:15 -0300

    Remove listaAlunos, professorTitular e professorAdjunto do construtor.

--- src/Curso.kt
+++ src/Curso.kt
@@ -1,11 +1,11 @@
 class Curso(
     val nome: String,
     val codigoDoCurso: Int,
-    val professorTitular: ProfessorTitular,
-    val professorAdjunto: ProfessorAdjunto,
     val qtdMaxima: Int,
-    private var listaAlunos: MutableList<Aluno> = mutableListOf()
 ) {
+    var professorTitular: ProfessorTitular? = null
+    var professorAdjunto: ProfessorAdjunto? = null
+    private var listaAlunos: MutableList<Aluno> = mutableListOf()
 
     override fun equals(other: Any?): Boolean {
         return (other is Curso) && this.codigoDoCurso == other.codigoDoCurso
