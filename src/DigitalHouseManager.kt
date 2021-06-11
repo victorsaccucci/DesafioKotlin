443d34fe0afc1ff0fcf059bfadcff2f0ea71ffca
victor saccucci <victorsaccucci@hotmail.com>
2021-06-11 16:30:30 -0300

    criada a função registrarCurso.

--- src/DigitalHouseManager.kt
+++ src/DigitalHouseManager.kt
@@ -4,4 +4,23 @@ class DigitalHouseManager {
     var listaDeCursos: MutableList<Curso> = mutableListOf()
     var listaDeMatriculas: MutableList<Matricula> = mutableListOf()
 
+    fun registrarCurso(nome: String, codigoDoCurso: Int, qtdMaxima: Int ){
+        val curso = Curso(nome, codigoDoCurso, qtdMaxima)
+        listaDeCursos.add(curso)
+    }
+
+    fun excluirCurso(codigoDoCurso: Int){
+    }
+
+    fun registrarProfessorAdjunto(nome: String, sobrenome: String, codigo: Int, horasDeMonitoria: Int)
+
+    fun registrarProfessorTitular(nome: String, sobrenome: String, codigo: Int, especialidade: String)
+
+    fun excluirProfessor(codigo: Integer)
+
+    fun registrarAluno(nome: String, sobrenome: String, codigoAluno: Int)
+
+    fun matricularAluno(codigoAluno: Int, codigoDoCurso: Int)
+
+    fun alocarProfessores(codigoDoCurso: Int, codigoProfessorTitular: Int, codigoProfessorAdjunto: Int)
 }
